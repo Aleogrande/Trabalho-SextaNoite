409dfe0cf04369931b8333263cebe4ce23a18467
Aleogrande <hipolitoalexandreribeiro@gmail.com>
2025-04-11 20:48:57 -0300

    Add files via upload

---
+++ Exercicio 1/NomeeSaudacao/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+public class NomeeSaudacao
+{
+    public static void Main(string[] args)
+    {
+    Console.WriteLine("Digite seu nome:");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine($"Olá, {nome}! Seja bem-vindo!");
+    }
+}
