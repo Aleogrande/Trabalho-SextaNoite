8db70075ecf62197627bb6304cd66c74258d96a0
Aleogrande <hipolitoalexandreribeiro@gmail.com>
2025-04-11 20:49:38 -0300

    Add files via upload

---
+++ Exercicio 2/CalculandoaIdade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class CalculandoaIdade
+{
+    public static void Main(string[] args)
+    {
+        int anoAtual = 2024;
+
+        Console.WriteLine("Por favor, digite o seu ano de nascimento:");
+        string anoNascimentoString = Console.ReadLine();
+
+
+        if (int.TryParse(anoNascimentoString, out int anoNascimento))
+        {
+
+            if (anoNascimento > 0 && anoNascimento <= anoAtual)
+            {
+                int idade = anoAtual - anoNascimento;
+                Console.WriteLine($"Considerando o ano atual como {anoAtual}, sua idade é: {idade} anos.");
+            }
+        }
+    }
+}
